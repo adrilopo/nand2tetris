f8a450e69d199b678d027f89cac56819beeb59ed
adrilopo <adrilopo@adrilopo.com>
2018-12-10 01:22:43 -0800

    Project 3B and 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,51 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LISTEN)
+  @KBD
+  D=M
+  @CLEAR
+  D;JEQ  // If KBD=0 goto CLEAR
+  @BLACK
+  0;JMP  // Otherwise goto BLACK
+
+(BLACK)
+  @count
+  M=0    // Initialize count=0
+(LOOP-BLACK)
+  @count
+  D=M  // D=count
+  @8191
+  D=A-D  // D=8191-count (The screen consists of 256*32=8192 16-bit words). 
+  @LISTEN
+  D; JLT  // If count>8192 goto LISTEN
+  @count
+  D=M    // D=count
+  @SCREEN
+  A=D+A  // A=SCREEN+count
+  M=-1   // Set every pixel in the row to 1
+  @count 
+  M=M+1  // count=count-1
+  @LOOP-BLACK
+  0;JMP  // goto LOOP-BLACK
+
+(CLEAR)
+  @count
+  M=0
+(LOOP-CLEAR)
+  @count
+  D=M  // D=count
+  @8191
+  D=A-D  // D=8191-count (The screen consists of 256*32=8192 16-bit words).
+  @LISTEN
+  D; JLT  // If count>8191 goto LISTEN
+  @count
+  D=M    // D=count
+  @SCREEN
+  A=D+A  // A=SCREEN+count
+  M=0   // Set every pixel in the row to 0
+  @count 
+  M=M+1  // count=count-1
+  @LOOP-CLEAR
+  0;JMP  // goto LOOP-CLEAR
+   
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,21 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R2
+M=0   // Initialize R2=0
+(LOOP)
+  @R0
+  D=M  // D=R0
+  @END
+  D;JLE  // If R0 <=0 goto END
+  @R1
+  D=M  // D=R1
+  @R2
+  M=M+D  //R2=R2+R1
+  @R0
+  M=M-1  //R0=R0-1
+  @LOOP
+  0;JMP  // goto LOOP
+(END)
+  @END
+  0;JMP // infinite loop 
